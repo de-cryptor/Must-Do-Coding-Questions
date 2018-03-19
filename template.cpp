dce7420298320f796b1cbc2fdd2accbfc81a0d15
jatin58 <jatingoel58@gmail.com>
2018-03-19 14:59:16 +0530

    template file added

---
+++ template.cpp
@@ -0,0 +1,22 @@
+/* Jatin Goel
+	IIIT Allahabad*/
+/* There is no substitute of hardwork. */
+/* Hardwork definitely pays off. */
+/* There is no shortcut to success. */
+#include <bits/stdc++.h>
+using namespace std;
+#define  LL long long
+#define F first
+#define S second
+#define fast_io ios::sync_with_stdio(false);cin.tie(NULL)
+int main()
+{
+	fast_io;
+	if(fopen("input.txt", "r"))
+	{
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt","w",stdout);
+	}
+	
+
+}

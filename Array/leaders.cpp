450db30285830c4a02bac85c77c341d8e811b181
Jatin Goel <bim2014001@iiita.ac.in>
2018-04-02 01:40:35 +0530

    Leaders in the Array

--- Array/leaders.cpp
+++ Array/leaders.cpp
@@ -11,6 +11,27 @@ using namespace std;
 #define F first
 #define S second
 #define fast_io ios::sync_with_stdio(false);cin.tie(NULL)
+
+int leaders(int a[],int n)
+{
+	stack<int> s;
+	int mx = a[n-1];
+	for(int i=n-2;i>=0;i--)
+	{
+		if(a[i] > mx)
+		{
+			s.push(a[i]);
+			mx = a[i];
+		}
+	}
+	while(!s.empty())
+	{
+		cout << s.top() << " ";
+		s.pop();
+	}
+	cout << a[n-1] << " ";
+	cout << endl;
+}
 int main()
 {
 	fast_io;
@@ -19,6 +40,17 @@ int main()
 		freopen("input.txt", "r", stdin);
 		freopen("output.txt","w",stdout);
 	}
-	
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		int a[n+5];
+		for(int i=0;i<n;i++)
+			cin >> a[i];
+		leaders(a,n);
+
+	}
 
 }

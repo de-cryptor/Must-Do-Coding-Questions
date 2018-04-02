ab524877505889ede7c6f79bd03a0b682a3d3dba
Unknown <bim2014001@iiita.ac.in>
2018-04-02 22:36:47 +0530

    Chocolate Distribution Problem Done

--- Array/choclate_distribution_problem.cpp
+++ Array/choclate_distribution_problem.cpp
@@ -17,6 +17,22 @@ int main()
 	cin >> t;
 	while(t--)
 	{
-		
+		int n,k;
+		cin >> n;
+		int a[n];
+		int mn = INT_MAX;
+		for(int i=0;i<n;i++)
+		{
+			cin >> a[i];
+		}
+		cin >> k;
+		sort(a,a+n);
+		for(int i=0;i<n;i++)
+		{
+			if( (i+k-1 ) < n && (a[i+k-1] - a[i]) < mn )
+				mn = a[i+k-1] - a[i];
+
+		}
+		cout << mn << endl;
 	}
 }
